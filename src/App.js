c5aa76d908b8523240f5fe6d64cc60d7c17dea26
samuel00410 <samuel00410@gmail.com>
2024-04-16 16:57:37 +0800

    useMemo Hooks practice

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
+import React, { useState, useMemo } from "react";
+
 function App() {
+  const [number, setNumber] = useState(0);
+  const [dark, setDark] = useState(true);
+  const doubleNumber = useMemo(() => slowFunction(number), [number]);
+  const themeStyles = {
+    backgroundColor: dark ? "black" : "white",
+    color: dark ? "white" : "black",
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
-    </div>
+    <>
+      <input
+        type="number"
+        value={number}
+        onChange={(e) => setNumber(parseInt(e.target.value))}
+      />
+      <button onClick={() => setDark((prevDark) => !prevDark)}>改變主題</button>
+      <div style={themeStyles}>{doubleNumber}</div>
+    </>
   );
 }
+
+const slowFunction = (num) => {
+  console.log("呼叫 slowFunction");
+  for (let i = 0; i <= 1000000000; i++) {}
+  return num * 2;
+};
 
 export default App;
